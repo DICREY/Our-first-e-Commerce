1bfae0aae423d0664cb634c45f13fa5f40e90b8d
DICREY <dicreyraptor@gmail.com>
2025-06-06 14:30:35 -0500

    first

--- FrontEnd/components/Badge/Badge.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Badge.module.css"
-
-const Badge = ({ children, variant = "primary", className = "" }) => {
-  const badgeClasses = [styles.badge, styles[variant], className].filter(Boolean).join(" ")
-
-  return <span className={badgeClasses}>{children}</span>
-}
-
-export default Badge
