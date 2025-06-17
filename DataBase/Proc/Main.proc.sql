96decde19233f0d991a46fff9cb055f3eb490eab
DICREY <dicreyraptor@gmail.com>
2025-06-17 14:43:19 -0500

    the best dataBase

---
+++ DataBase/Proc/Main.proc.sql
@@ -0,0 +1,24 @@
+CREATE PROCEDURE e_commerce.Login(
+    IN p_firstData VARCHAR(100)
+)
+BEGIN
+    SELECT
+        p.nom_per,
+        p.ape_per,
+        p.doc_per,
+        p.cont_per,
+        p.fot_per,
+        GROUP_CONCAT(r.nom_rol SEPARATOR ', ') AS roles
+    FROM
+        personas p
+    JOIN
+        otorgar_roles otr ON otr.id_per = p.id_per
+    JOIN
+        roles r ON otr.id_rol = r.id_rol
+    WHERE
+        p.estado = 1
+        AND p.email_per = p_firstData
+    ORDER BY 
+        p.nom_per
+    LIMIT 1000;
+END //
