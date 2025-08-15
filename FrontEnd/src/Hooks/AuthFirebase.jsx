998627af52159c49ed02ec5241afa8ad6dcf2286
DICREY <dicreyraptor@gmail.com>
2025-08-15 18:40:44 -0500

    implementanto el login con google

---
+++ FrontEnd/src/Hooks/AuthFirebase.jsx
@@ -0,0 +1,21 @@
+// Librarys 
+import 'firebase/auth'
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth';
+
+// firebase project config
+const firebaseConfig = {
+  apiKey: "AIzaSyCy-JZIwev-ss3dbRkyKTg90IeYMCCuzQ4",
+  authDomain: "e-commerce-the-four.firebaseapp.com",
+  projectId: "e-commerce-the-four",
+  storageBucket: "e-commerce-the-four.firebasestorage.app",
+  messagingSenderId: "1021124126793",
+  appId: "1:1021124126793:web:aafb4f8dd08636e827b798",
+  measurementId: "G-01R0TW1E8Q"
+}
+
+// Inicializar project app
+const app = initializeApp(firebaseConfig)
+
+// Get Project App
+export const auth = getAuth(app)
